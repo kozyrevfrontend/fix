403821852b448d3b981088ac63372b83e2e5cbad
Kozyrev Ilya <kozyrev.frontend@gmail.com>
2021-03-01 22:48:42 +0300

    добавляет логику для блока video

--- build/js/main.js
+++ build/js/main.js
@@ -68,8 +68,87 @@
 		}
 	}
 
+	class Video
+	{
+		constructor(domNode)
+		{
+			this.container = domNode.querySelector('.video__container');
+			this.link = this.container.querySelector('.video__link');
+			this.button = this.container.querySelector('.video__button');
+
+			this.setupVideo = this.setupVideo.bind(this);
+		}
+
+		init() {
+			this.setupVideo();
+		}
+
+		setupVideo() {
+			this.link.removeAttribute('href');
+
+			this.button.addEventListener('click', () => {
+				const iframe = this.createIframe();
+				this.link.remove();
+				this.button.remove();
+				this.container.appendChild(iframe);
+			});
+		}
+
+		createIframe() {
+			const iframe = document.createElement('iframe');
+			iframe.setAttribute('src', 'https://www.youtube.com/embed/TEBpc9Q9TV8');
+			iframe.setAttribute('allowfullscreen', '');
+			iframe.classList.add('video__media');
+
+			return iframe;
+		}
+	}
+
+	// function findVideo() {
+	// 	var video = document.querySelector('.video__container');
+	//
+	// 	if (video) {
+	// 		setupVideo(video);
+	// 	}
+	// }
+	//
+	// function setupVideo(video) {
+	// 	var link = document.querySelector('.video__link');
+	//
+	// 	if (link) {
+	// 		link.removeAttribute('href');
+	// 	}
+	//
+	// 	var button = document.querySelector('.video__button');
+	//
+	// 	if (button) {
+	// 		button.addEventListener('click', function () {
+	// 			var iframe = createIframe();
+	// 			link.remove();
+	// 			button.remove();
+	// 			video.appendChild(iframe);
+	// 		});
+	// 	}
+	//
+	// 	video.classList.add('video__container--enabled');
+	// }
+	//
+	// function createIframe() {
+	// 	var iframe = document.createElement('iframe');
+	// 	iframe.setAttribute('src', 'https://www.youtube.com/embed/bnzHECC0Z8A?rel=0&showinfo=0&autoplay=1');
+	// 	iframe.setAttribute('allowfullscreen', '');
+	// 	iframe.classList.add('video__media');
+	//
+	// 	return iframe;
+	// }
+	//
+	// findVideo();
+
 	const slider = new Slider(document.querySelector('.designs'));
 	slider.init();
+
+	const video = new Video(document.querySelector('.header__video'));
+	video.init();
 
 }());
 
--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,8 @@
 import Slider from './components/slider';
+import Video from "./components/video";
 
 const slider = new Slider(document.querySelector('.designs'));
 slider.init();
+
+const video = new Video(document.querySelector('.header__video'));
+video.init();
